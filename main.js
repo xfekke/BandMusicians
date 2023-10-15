323901e5b0096f2b42007794ec77e9f65f6a3e64
xfekke <fekke4201337@gmail.com>
2023-10-15 17:03:46 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 const fs = require('fs');
 const Band = require('./band');
 const Musician = require('./musician');
+
+const savedInfo = 'savedInfo.json'
+
+function saveData() {
+  fs.writeFileSync(savedInfo, JSON.stringify(null, 2));
+}
