41c13457b93e0e971361c6ffc22e0d713032d30b
xfekke <fekke4201337@gmail.com>
2023-10-16 19:38:42 +0200

    Update

--- main.js
+++ main.js
@@ -5,16 +5,12 @@
 //Band kan vara arvgivare till musiker
 //JSON för band, JSON för musiker
 
-//import PromptSync from "prompt-sync";
-import { Musician } from "./musician";
-import { Band } from "./band"
-const prompt = PromptSync({ sigint: true})
+import PromptSync from "prompt-sync";
+import { Musician } from "./musician.js";
+import { Band } from "./band.js"
+const prompt = PromptSync({ sigint: true })
 
-//switch break, ta funktioner från js samt klasser. Spara i JSON. 
-
-
-
-
+//switch break, ta funktioner från js samt klasser. Spara i JSON.
 const musicianInfo = 'musicianInfo.json'
 const bandInfo = 'bandInfo.json'
 /*
@@ -23,4 +19,24 @@ function saveData() {
 }
 */
 console.log("Hello! What would you like to do?")
-console.log(Musician)
+console.log("1. Create band")
+console.log("2. Remove band")
+console.log("3. Create musician")
+console.log("4. Remove musician")
+console.log("5. Show bands")
+console.log("6. Show musicians")
+
+let menyOption = prompt("Enter your option - ")
+
+switch (menyOption) {
+  case "1"
+    
+    break;
+
+  case "2":
+
+    break;
+
+  default:
+    console.log("You did not enter a valid option.")
+}
