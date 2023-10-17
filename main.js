cb6ca604d62379bc067760517913030311b49e8a
xfekke <fekke4201337@gmail.com>
2023-10-17 07:03:26 +0200

    Musician.js Update

--- main.js
+++ main.js
@@ -5,28 +5,32 @@
 //Band kan vara arvgivare till musiker
 //JSON för band, JSON för musiker
 //while true funktion för att se till att namn prompts är 2 eller fler bokstäver
-
-import PromptSync from "prompt-sync";
-import { Musician, createMusician } from "./musician.js";
-import { Band } from "./band.js"
-import { Musician } from "./musician.js";
-const prompt = PromptSync({ sigint: true })
-
-//switch break, ta funktioner från js samt klasser. Spara i JSON.
-const musicianInfo = 'musicianInfo.json'
-const bandInfo = 'bandInfo.json'
 /*
 function saveData() {
   fs.writeFileSync(savedInfo, JSON.stringify(null, 2));
 }
 */
+
+import PromptSync from "prompt-sync";
+import { Musician, createMusician } from "./musician.js";
+import { Band } from "./band.js"
+import fs from "fs";
+const prompt = PromptSync({ sigint: true })
+
+//switch break, ta funktioner från js samt klasser. Spara i JSON.
+const musicianInfoData = 'musicianInfo.json';
+const bandInfoData = 'bandInfo.json';
+
+
+
+
 console.log("Hello! What would you like to do?")
-console.log("3. Create musician")
-console.log("6. Show musicians")
-console.log("4. Remove musician")
-console.log("1. Create band")
+console.log("1. Create musician")
+console.log("2. Show musicians")
+console.log("3. Remove musician")
+console.log("4. Create band")
 console.log("5. Show bands")
-console.log("2. Remove band")
+console.log("6. Remove band")
 
 
 
@@ -37,6 +41,9 @@ switch (menyOption) {
     const newMusician = createMusician();
     console.log('New musician has been created' + newMusician.name)
     console.log(newMusician)
+
+    const musicianData = JSON.stringify(newMusician, null, 2);
+    fs.writeFileSync(musicianInfoData, musicianData)
     break;
 
   case "2":
--- musician.js
+++ musician.js
@@ -13,15 +13,46 @@ import PromptSync from "prompt-sync";
 const prompt = PromptSync({ sigint: true })
 
 export function createMusician() {
-  const name = prompt("Enter the musicians name - ")
-  const info = prompt("Enter any info about the musician here - ")
-  const birthYear = prompt("Enter the musicians birthyear (yyyy-mm-dd) - ")
-  const bands = prompt("Enter the musicians band - ")
-  const formerBands = prompt("Enter the musicians former bands if he/she had any - ")
-  const instruments = prompt("Enter the musicians instrument (singing counts) - ")
+  let name = '';
+  let birthYear = '';
+  let bands = '';
+  let instruments = '';
 
-  return new Musician(name, info, birthYear, bands, formerBands, instruments)
+  do {
+    name = prompt("Enter the musician's name - ");
+    if (name.length <= 1) {
+      console.log("You have to enter at least 2 characters.");
+    }
+  } while (name.length <= 1);
+
+  const info = prompt("Enter any info about the musician here if you want - ");
+
+  do {
+    birthYear = prompt("Enter the musician's birthyear (yyyy-mm-dd) - ");
+    if (birthYear.length !== 8 || isNaN(birthYear)) {
+      console.log("Enter the birthyear in 8 numbers (yyyy-mm-dd) - ");
+    }
+  } while (birthYear.length !== 8 || isNaN(birthYear));
+
+  do {
+    bands = prompt("Enter the musician's bands (separate with commas) - ");
+    if (bands.length <= 1) {
+      console.log("You have to enter at least 2 characters.");
+    }
+  } while (bands.length <= 1);
+
+  const formerBands = prompt("Enter the musician's former bands if he/she had any - ");
+
+  do {
+    instruments = prompt("Enter the musician's instruments (separate with commas) - ");
+    if (instruments.length <= 1) {
+      console.log("You have to enter at least 2 characters.");
+    }
+  } while (instruments.length <= 1);
+
+  return new Musician(name, info, birthYear, bands.split(","), formerBands, instruments.split(","));
 }
+
 
 
 
